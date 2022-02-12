0d69be053c8029f681a24419e1199b7f868ab0a9
kdot <alvarezderrick0@gmail.com>
2022-02-12 14:39:04 -0800

    updated file structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
+import PortfolioContainer from "./components/PortfolioContainer";
+
+// import Hero from "./components/Hero";
+// import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <Hero />
-      <Navbar />
-    </div>
+   <PortfolioContainer />
   );
 }
 
