b3fcb0aa0dcbb38040ed4e3fbc3db0ead3fd6ce2
kdot <alvarezderrick0@gmail.com>
2022-02-12 19:46:53 -0800

    updated imports

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
