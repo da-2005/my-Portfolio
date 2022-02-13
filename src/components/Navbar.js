92662bab151e65ab3c214a3a653c52baa7add3f8
kdot <alvarezderrick0@gmail.com>
2022-02-12 19:45:53 -0800

    Home tab is linked to my hero page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,21 +4,21 @@ import React from 'react'
 
 export default function Navbar({currentPage, handlePageChange}) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#Hero" onClick={() => handlePageChange('Hero')}>Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
-        <a className="nav-link disabled">Disabled</a>
-      </div>
-    </div>
-  </div>
-</nav>
-    );
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <p className="navbar-brand">Derrick's Portfolio</p>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className={currentPage === 'Hero' ? 'nav-link active' : 'nav-link'} href="#Hero" onClick={() => handlePageChange('Hero')}>Home</a>
+              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link disabled">Disabled</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+  );
 }
