9a76065b182ce7ee71990add9074d23afb50aa58
kdot <alvarezderrick0@gmail.com>
2022-02-14 19:00:45 -0800

    added path to About Me page

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,32 +1,30 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar';
 import Hero from './pages/Hero';
+import About from './pages/About';
 
 
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Hero');
   
-    // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
     const renderPage = () => {
       if (currentPage === 'Hero') {
         return <Hero />;
       }
-    //   if (currentPage === 'About') {
-    //     return <About />;
-    //   }
-    //   if (currentPage === 'Blog') {
-    //     return <Blog />;
-    //   }
-    //   return <Contact />;
+      if (currentPage === 'About') {
+        return <About />;
+      }
+      // if (currentPage === 'Projects') {
+      //   return <Projects />;
+      // }
+      // return <Contact />;
     };
   
     const handlePageChange = (page) => setCurrentPage(page);
   
     return (
       <div>
-        {/* We are passing the currentPage from state and the function to update it */}
         <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-        {/* Here we are calling the renderPage method which will return a component  */}
         {renderPage()}
       </div>
     );
